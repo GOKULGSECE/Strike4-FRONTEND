bd3411a2e0a3f57b9c7e1d80d601ca1660170f5d
tamilkumaran <tamilkumaran1494@gmail.com>
2025-03-08 15:41:36 +0530

    intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Login from './components/Login';
-import SignUp from './components/SignUp'
+import SignUp from './components/SignUp';
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
@@ -16,7 +16,6 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
-    
   );
 }
 
